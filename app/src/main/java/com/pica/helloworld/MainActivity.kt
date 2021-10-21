0623298ad4fe2acd6cb36b3cef10a93c6776d90e
Paul Webb <pauwebb@synamedia.com>
2021-10-21 15:59:55 +0100

    Appcenter additiaons

--- app/src/main/java/com/pica/helloworld/MainActivity.kt
+++ app/src/main/java/com/pica/helloworld/MainActivity.kt
@@ -2,10 +2,17 @@ package com.pica.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCenter.start(
+            application, "61c18d13-d100-4bd9-8800-0f1e231f2e25",
+            Analytics::class.java, Crashes::class.java
+        )
         setContentView(R.layout.activity_main)
     }
 }
