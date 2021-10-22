8948e4eb52f6367f8282f35770c144e5c06a643a
Paul Webb <pauwebb@synamedia.com>
2021-10-22 09:28:04 +0100

    Upgrade Gradle via IDE

--- app/src/main/java/com/pica/helloworld/MainActivity.kt
+++ app/src/main/java/com/pica/helloworld/MainActivity.kt
@@ -8,11 +8,11 @@ import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         AppCenter.start(
             application, "61c18d13-d100-4bd9-8800-0f1e231f2e25",
             Analytics::class.java, Crashes::class.java
         )
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
